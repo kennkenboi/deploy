0cb1a269d8edfeae2300922dc75e9d8a3aa9f454
kenboi <kennkenboi@gmail.com>
2020-05-12 19:51:34 +0300

    Fixed command syntax errors and SSH errors causing deployments to fail

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
-FROM alpine:3.11.3
+FROM alpine:latest
 
-MAINTAINER Hoang Ngoc Quy <ngocquyhoang3112@gmail.com>
+MAINTAINER Kenboi <kennkenboi+docker@gmail.com>
 
 RUN apk update \
 	&& apk upgrade \
-	&& apk add --no-cache rsync openssh-client \
+	&& apk add --no-cache dos2unix openssh-client \
 	&& rm -rf /var/cache/apk/*
+
+ENV DEPLOY_HOST=80.240.17.13
+ENV DEPLOY_USER=root
+ENV DEPLOY_PATH=/var/www/html/testai-api
 
 COPY entrypoint.sh /script/entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 echo $'\n' "------ CHECKING FOR CONFIGURATION KEYS! ----------------" $'\n'
 
-if [ -z "$DEPLOY_KEY" ]; then
+if [ -z "$DEPLOY_KEY" ] 
+then 
 	echo $'\n' "------ DEPLOY KEY NOT SET YET! ----------------" $'\n'
 	exit 1
 fi
@@ -22,6 +23,9 @@ if [ -z "$DEPLOY_PATH" ]; then
 	exit 1
 fi
 
+echo $'\n' "------ DEPLOYMENT KEYS CHECK COMPLETE ----------------" $'\n'
+
+
 #update known hosts
 mkdir -p /root/.ssh
 ssh-keyscan -H "$DEPLOY_HOST" >>/root/.ssh/known_hosts
@@ -30,21 +34,20 @@ ssh-keyscan -H "$DEPLOY_HOST" >>/root/.ssh/known_hosts
 printf '%b\n' "$DEPLOY_KEY" >/root/.ssh/id_rsa
 chmod 400 /root/.ssh/id_rsa
 
-echo $'\n' "------ CONFIG SUCCESSFUL! ---------------------" $'\n'
-
+echo $'\n' "------ SSH KEY DEPLOYED SUCCESSFUL! ---------------------" $'\n'
 
 #Test the connection
-MSG=$(ssh -i /root/.ssh/id_rsa -q $DEPLOY_USER@$DEPLOY_HOST)
+ssh -q -i /root/.ssh/id_rsa $DEPLOY_USER@$DEPLOY_HOST
 if [ $? -eq 0 ]; then
-	echo $'\n' "------ CONNECTION SUCCESSFUL! -----------------------" $'\n'
+	echo $'\n' "------ CONNECTION TEST SUCCESSFUL! -----------------------" $'\n'
 else
-	echo $'\n' "------ CONNECTION FAILED! $HOST $USER -----------------------" $'\n'
-	echo MSG
+	echo $'\n' "------ CONNECTION TEST FAILED! -----------------------" $'\n'
 	exit 1
 fi
 
+
 #PULL
-ssh -i /root/.ssh/id_rsa -t $DEPLOY_USER@$DEPLOY_HOST "git --git-dir=$DEPLOY_PATH pull"
+ssh -i /root/.ssh/id_rsa -t $DEPLOY_USER@$DEPLOY_HOST "cd $DEPLOY_PATH && git pull"
 if [ $? -ne 0 ]; then
 	echo $'\n' "------ UNABLE TO PULL CODE! -----------------------" $'\n'
 	exit 1
@@ -52,19 +55,20 @@ fi
 echo $'\n' "------ CODE PULLED! -----------------------" $'\n'
 
 #composer
-ssh -i /root/.ssh/id_rsa -t $DEPLOY_USER@$DEPLOY_HOST "composer install -q -n -d=$DEPLOY_PATH"
+ssh -i /root/.ssh/id_rsa -t $DEPLOY_USER@$DEPLOY_HOST "composer install -q -n -d $DEPLOY_PATH"
 if [ $? -ne 0 ]; then
 	echo $'\n' "------ COMPOSER INSTALL! FAILED! -----------------------" $'\n'
 	exit 1
 fi
-echo $'\n' "------ COMPOSER INSTALL! -----------------------" $'\n'
+echo $'\n' "------ COMPOSER INSTALL SUCCESSFUL! -----------------------" $'\n'
 
 #composer dump autoload
-ssh -i /root/.ssh/id_rsa -t $DEPLOY_USER@$DEPLOY_HOST "composer dump-autoload -o -q -n -d=$DEPLOY_PATH"
-echo $'\n' "------ COMPOSER INSTALL! -----------------------" $'\n'
+ssh -i /root/.ssh/id_rsa -t $DEPLOY_USER@$DEPLOY_HOST "composer dump-autoload -o -q -n -d $DEPLOY_PATH"
+echo $'\n' "------ COMPOSER DUMPAUTOLOAD SUCCESSFUL! -----------------------" $'\n'
+
 
 #config cache
-ssh -i /root/.ssh/id_rsa -t $DEPLOY_USER@$DEPLOY_HOST "php $DEPLOY_PATH/artisan config:cache"
+ssh -i /root/.ssh/id_rsa -t $DEPLOY_USER@$DEPLOY_HOST "cd $DEPLOY_PATH && php artisan config:cache"
 if [ $? -ne 0 ]; then
 	echo $'\n' "------ CACHE CONFIG FAILED! -----------------------" $'\n'
 	exit 1
@@ -72,12 +76,15 @@ fi
 echo $'\n' "------ CONFIG CACHE CLEARED AND UPDATED! -----------------------" $'\n'
 
 #run migrations
-ssh -i /root/.ssh/id_rsa -t $DEPLOY_USER@$DEPLOY_HOST "php $DEPLOY_PATH/artisan migrate"
+ssh -i /root/.ssh/id_rsa -t $DEPLOY_USER@$DEPLOY_HOST "cd $DEPLOY_PATH && php artisan migrate"
 if [ $? -ne 0 ]; then
 	echo $'\n' "------ MIGRATIONS FAILED! -----------------------" $'\n'
 	exit 1
 fi
+
 echo $'\n' "------ MIGRATIONS COMPLETED! -----------------------" $'\n'
 
-echo $'\n' "------ CONGRATS! DEPLOY SUCCESSFUL!!! ---------" $'\n'
-exit 0
+echo $'\n' "------ CONGRATULATIONS! THE DEPLOYMENT WAS SUCCESSFUL!!! ---------" $'\n'
+
+
+exit 0;
