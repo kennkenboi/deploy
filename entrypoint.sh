2736594ad29e4898c3c14e11a0bd8b7b2278b567
kenboi <kennkenboi@gmail.com>
2020-05-12 15:57:18 +0300

    Added modifications for git only use

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,44 +1,81 @@
 #!/bin/bash
 
 mkdir -p /root/.ssh
-ssh-keyscan -H "$2" >> /root/.ssh/known_hosts
+ssh-keyscan -H "$2" >>/root/.ssh/known_hosts
 
-if [ -z "$DEPLOY_KEY" ];
-then
+echo $'\n' "------ CHECKING FOR CONFIGURATION KEYS! ----------------" $'\n'
+
+if [ -z "$DEPLOY_KEY" ]; then
 	echo $'\n' "------ DEPLOY KEY NOT SET YET! ----------------" $'\n'
 	exit 1
-else
-	printf '%b\n' "$DEPLOY_KEY" > /root/.ssh/id_rsa
-	chmod 400 /root/.ssh/id_rsa
-
-	echo $'\n' "------ CONFIG SUCCESSFUL! ---------------------" $'\n'
 fi
 
-rsync --progress -avzh \
-	--exclude='.git/' \
-	--exclude='.git*' \
-	--exclude='.editorconfig' \
-	--exclude='.styleci.yml' \
-	--exclude='.idea/' \
-	--exclude='Dockerfile' \
-	--exclude='readme.md' \
-	--exclude='README.md' \
-	-e "ssh -i /root/.ssh/id_rsa" \
-	--rsync-path="sudo rsync" . $1@$2:$3
-
-if [ $? -eq 0 ]
-then
-	echo $'\n' "------ SYNC SUCCESSFUL! -----------------------" $'\n'
-	echo $'\n' "------ RELOADING PERMISSION -------------------" $'\n'
-
-	ssh -i /root/.ssh/id_rsa -t $1@$2 "sudo chown -R $4:$4 $3"
-	ssh -i /root/.ssh/id_rsa -t $1@$2 "sudo chmod 775 -R $3"
-	ssh -i /root/.ssh/id_rsa -t $1@$2 "sudo chmod 777 -R $3/storage"
-	ssh -i /root/.ssh/id_rsa -t $1@$2 "sudo chmod 777 -R $3/public"
-
-	echo $'\n' "------ CONGRATS! DEPLOY SUCCESSFUL!!! ---------" $'\n'
-	exit 0
-else
-	echo $'\n' "------ DEPLOY FAILED! -------------------------" $'\n'
+if [ -z "$USER" ]; then
+	echo $'\n' "------ USER IS NOT SET YET! ----------------" $'\n'
 	exit 1
 fi
+
+if [ -z "$HOST" ]; then
+	echo $'\n' "------ HOST IS NOT SET YET! ----------------" $'\n'
+	exit 1
+fi
+
+if [ -z "$PATH" ]; then
+	echo $'\n' "------ PATH IS NOT SET YET! ----------------" $'\n'
+	exit 1
+fi
+
+#deploy ssh key
+printf '%b\n' "$DEPLOY_KEY" >/root/.ssh/id_rsa
+chmod 400 /root/.ssh/id_rsa
+
+echo $'\n' "------ CONFIG SUCCESSFUL! ---------------------" $'\n'
+
+
+#Test the connection
+ssh -i /root/.ssh/id_rsa -q $USER@$HOST
+if [ $? -eq 0 ]; then
+	echo $'\n' "------ CONNECTION SUCCESSFUL! -----------------------" $'\n'
+else
+	echo $'\n' "------ CONNECTION FAILED! -----------------------" $'\n'
+	exit 1
+fi
+
+#PULL
+ssh -i /root/.ssh/id_rsa -t $USER@$HOST "git --git-dir=$PATH pull"
+if [ $? -ne 0 ]; then
+	echo $'\n' "------ UNABLE TO PULL CODE! -----------------------" $'\n'
+	exit 1
+fi
+echo $'\n' "------ CODE PULLED! -----------------------" $'\n'
+
+#composer
+ssh -i /root/.ssh/id_rsa -t $USER@$HOST "composer install -q -n -d=$PATH"
+if [ $? -ne 0 ]; then
+	echo $'\n' "------ COMPOSER INSTALL! FAILED! -----------------------" $'\n'
+	exit 1
+fi
+echo $'\n' "------ COMPOSER INSTALL! -----------------------" $'\n'
+
+#composer dump autoload
+ssh -i /root/.ssh/id_rsa -t $USER@$HOST "composer dump-autoload -o -q -n -d=$PATH"
+echo $'\n' "------ COMPOSER INSTALL! -----------------------" $'\n'
+
+#config cache
+ssh -i /root/.ssh/id_rsa -t $USER@$HOST "php $PATH/artisan config:cache"
+if [ $? -ne 0 ]; then
+	echo $'\n' "------ CACHE CONFIG FAILED! -----------------------" $'\n'
+	exit 1
+fi
+echo $'\n' "------ CONFIG CACHE CLEARED AND UPDATED! -----------------------" $'\n'
+
+#run migrations
+ssh -i /root/.ssh/id_rsa -t $USER@$HOST "php $PATH/artisan migrate"
+if [ $? -ne 0 ]; then
+	echo $'\n' "------ MIGRATIONS FAILED! -----------------------" $'\n'
+	exit 1
+fi
+echo $'\n' "------ MIGRATIONS COMPLETED! -----------------------" $'\n'
+
+echo $'\n' "------ CONGRATS! DEPLOY SUCCESSFUL!!! ---------" $'\n'
+exit 0
